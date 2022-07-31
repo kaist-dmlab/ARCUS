8f6470ec9f264245f0fa19db4803c71b7c5c3d83
e0jun <youngjun.lee@kaist.ac.kr>
2022-07-31 17:23:00 +0900

    [Fix] __ARCUS for arguments

--- __ARCUS.py
+++ __ARCUS.py
@@ -1,48 +1,58 @@
 import tensorflow as tf
-import pandas as pd
 import numpy as np
-from copy import deepcopy
 from sklearn import metrics
-from CKA import linear_CKA, kernel_CKA
+from CKA import linear_CKA
 from model.model_utils import ModelGenerator
 
 class ARCUS:
-    def __init__(self, args, ModelGenerator):
-        self.args = args
-        self.args.itr_num = int(args.batch/args.min_batch)
-        # set model generator for initializing models
-        self.model_generator = ModelGenerator(args)
-        
+    def __init__(self, args):
+        self.seed = args.seed
 
+        self._model_type = args.model_type
+        self._inf_type   = args.inf_type
+        self._itr_num    = int(args.batch_size/args.min_batch_size)
+
+        self._batch_size = args.batch_size
+        self._min_batch_size    = args.min_batch_size
+        
+        self._init_epoch = args.init_epoch
+        self._intm_epoch = args.intm_epoch
+
+        self._reliability_thred = args.reliability_thred
+        self._similarity_thred  = args.similarity_thred
+
+        # set model generator for initializing models
+        self._model_generator = ModelGenerator(args)
+        
     def _standardize_scores(self, score: float):
         # get standardized anomaly scores
         mean_score = np.mean(score)
-        std_score = np.std(score)
-        standardized_score = np.array([(k-mean_score)/std_score for k in score])
+        std_score  = np.std(score)
+
+        standardized_score = np.array([(k-mean_score) / std_score for k in score])
         return standardized_score
     
-
     def _merge_models(self, 
                       model1: tf.keras.Model, 
                       model2: tf.keras.Model):
         # merge a previous model and a current model 
         num_batch_sum = model1.num_batch + model2.num_batch
-        w1 = model1.num_batch/num_batch_sum
-        w2 = model2.num_batch/num_batch_sum
+        w1 = model1.num_batch / num_batch_sum
+        w2 = model2.num_batch / num_batch_sum
         
         # merge encoder
         for layer_idx in range(len(model2.encoder)):
             l_base = model1.encoder[layer_idx]
             l_target = model2.encoder[layer_idx]
             if l_base.name[:5] == 'layer':
-                new_weight = (l_base.weights[0]*w1 + l_target.weights[0]*w2)
-                new_bias = (l_base.weights[1]*w1 + l_target.weights[1]*w2)
+                new_weight = (l_base.weights[0] * w1 + l_target.weights[0] * w2)
+                new_bias = (l_base.weights[1] * w1 + l_target.weights[1] * w2)
                 l_target.set_weights([new_weight, new_bias])
             elif l_base.name[:2] == 'bn':
-                new_gamma = (l_base.weights[0]*w1 + l_target.weights[0]*w2)
-                new_beta = (l_base.weights[1]*w1 + l_target.weights[1]*w2)
-                new_mm = (l_base.weights[2]*w1 + l_target.weights[2]*w2)
-                new_mv = (l_base.weights[3]*w1 + l_target.weights[3]*w2)
+                new_gamma = (l_base.weights[0] * w1 + l_target.weights[0] * w2)
+                new_beta = (l_base.weights[1] * w1 + l_target.weights[1] * w2)
+                new_mm = (l_base.weights[2] * w1 + l_target.weights[2] * w2)
+                new_mv = (l_base.weights[3] * w1 + l_target.weights[3] * w2)
                 l_target.set_weights([new_gamma, new_beta, new_mm, new_mv])
 
         # merge decoder
@@ -50,26 +60,23 @@ class ARCUS:
             l_base = model1.decoder[layer_idx]
             l_target = model2.decoder[layer_idx]
             if l_base.name[:5] == 'layer':
-                new_weight = (l_base.weights[0]*w1 + l_target.weights[0]*w2)
-                new_bias = (l_base.weights[1]*w1 + l_target.weights[1]*w2)
+                new_weight = (l_base.weights[0] * w1 + l_target.weights[0]*w2)
+                new_bias = (l_base.weights[1] * w1 + l_target.weights[1]*w2)
                 l_target.set_weights([new_weight, new_bias])
             elif l_base.name[:2] == 'bn':
-                new_gamma = (l_base.weights[0]*w1 + l_target.weights[0]*w2)
-                new_beta = (l_base.weights[1]*w1 + l_target.weights[1]*w2)
-                new_mm = (l_base.weights[2]*w1 + l_target.weights[2]*w2)
-                new_mv = (l_base.weights[3]*w1 + l_target.weights[3]*w2)
+                new_gamma = (l_base.weights[0] * w1 + l_target.weights[0] * w2)
+                new_beta = (l_base.weights[1] * w1 + l_target.weights[1] * w2)
+                new_mm = (l_base.weights[2] * w1 + l_target.weights[2] * w2)
+                new_mv = (l_base.weights[3] * w1 + l_target.weights[3] * w2)
                 l_target.set_weights([new_gamma, new_beta, new_mm, new_mv])
         
-        if self.args.model_type == 'RSRAE':
-            model2.A = (model1.A*w1 + model2.A*w2)
+        if self._model_type == 'RSRAE':
+            model2.A = (model1.A * w1 + model2.A * w2)
 
         model2.num_batch = num_batch_sum
         return model2
 
-    def _reduce_models_last(self, 
-                            x_inp,
-                            thred: float, 
-                            epoch_num: int):
+    def _reduce_models_last(self, x_inp, epochs):
         # delete similar models for reducing redundancy in a model pool
         latents = []
         for m in self.model_pool:
@@ -85,24 +92,20 @@ class ARCUS:
                 max_CKA = CKA
                 max_Idx1 = idx1
 
-        if max_Idx1 != None and max_CKA >= thred:
+        if max_Idx1 != None and max_CKA >= self._similarity_thred:
             self.model_pool[max_Idx2] = self._merge_models(self.model_pool[max_Idx1], self.model_pool[max_Idx2])
-            self._train_model(self.model_pool[max_Idx2], x_inp, epoch_num) #Train just one epoch to get the latest score info
+            self._train_model(self.model_pool[max_Idx2], x_inp, epochs) # Train just one epoch to get the latest score info
 
             self.model_pool.remove(self.model_pool[max_Idx1])       
             if len(self.model_pool) > 1:
-                self._reduce_models_last(self.model_pool, x_inp, thred, epoch_num)
+                self._reduce_models_last(self.model_pool, x_inp, epochs)
 
-
-    def _train_model(self, 
-                     model: tf.keras.Model, 
-                     x_inp,
-                     epoch_num):
+    def _train_model(self, model: tf.keras.Model, x_inp, epochs):
         # train a model in the model pool of ARCUS
         tmp_losses = []
-        for _ in range(epoch_num):
-            for _ in range(self.args.itr_num):
-                min_batch_x_inp = tf.random.shuffle(x_inp)[:self.args.min_batch]
+        for _ in range(epochs):
+            for _ in range(self._itr_num):
+                min_batch_x_inp = tf.random.shuffle(x_inp)[:self._min_batch_size]
                 loss = model.train_step(min_batch_x_inp)
                 tmp_losses.append(loss.numpy())
         temp_scores = model.inference_step(x_inp)
@@ -113,16 +116,11 @@ class ARCUS:
         
         return tmp_losses
 
-    def simulator(self, 
-                  dataset: tf.data.Dataset, 
-                  inf_type: str, 
-                  batch: int, 
-                  reliability_thred: float, 
-                  similarity_thred: float):
+    def simulator(self, loader):
         # Simulator for online anomaly detection
         
-        initial_model = self.model_generator.init_model()
-        curr_model = deepcopy(initial_model)
+        initial_model = self._model_generator.init_model()
+        curr_model = initial_model
         
         self.model_pool = []
         self.model_pool.append(initial_model)
@@ -134,14 +132,14 @@ class ARCUS:
         
         # Scenario for online anomaly detection
         try:    
-            for step, (x_inp, y_inp) in enumerate(dataset.batch(batch)):
+            for step, (x_inp, y_inp) in enumerate(loader.batch(self._batch_size)):
                 # Initial model training
                 if step == 0:
-                    tmp_losses = self._train_model(initial_model, x_inp, self.args.init_epoch)
+                    tmp_losses = self._train_model(initial_model, x_inp, self._init_epoch)
                     losses = losses + tmp_losses
 
                 # Inference
-                if inf_type == "INC":
+                if self._inf_type == "INC":
                     final_scores = initial_model.inference_step(x_inp)
                 else:
                     scores = []
@@ -154,7 +152,7 @@ class ARCUS:
                         min_score = curr_min_score if curr_min_score < m.last_min_score else m.last_min_score
                         max_score = curr_max_score if curr_max_score > m.last_max_score else m.last_max_score
                         gap = np.abs(curr_mean_score - m.last_mean_score)
-                        reliability = np.round(np.exp(-2*gap*gap/((2/batch)*(max_score-min_score)*(max_score-min_score))),4)
+                        reliability = np.round(np.exp(-2*gap*gap/((2/self._batch_size)*(max_score-min_score)*(max_score-min_score))),4)
                         model_reliabilities.append(reliability)
 
                     curr_model_index = model_reliabilities.index(max(model_reliabilities))
@@ -172,11 +170,11 @@ class ARCUS:
                     auc_hist.append(auc)
 
                 #Drift detection
-                if inf_type == "INC": 
+                if self._inf_type == "INC": 
                     drift = False
                 else:
                     pool_reliability = 1-np.prod([1-p for p in model_reliabilities])
-                    if pool_reliability < reliability_thred:
+                    if pool_reliability < self._reliability_thred:
                         drift = True
                     else:
                         drift = False
@@ -185,17 +183,17 @@ class ARCUS:
                 if(drift):
                     drift_hist.append(step)
                     #Create new model
-                    new_model = self.model_generator.init_model()
-                    tmp_losses = self._train_model(new_model, x_inp, self.args.init_epoch)
+                    new_model = self._model_generator.init_model()
+                    tmp_losses = self._train_model(new_model, x_inp, self._init_epoch)
                     losses = losses + tmp_losses
                     self.model_pool.append(new_model)
                     #Merge models
-                    self.model_pool = self._reduce_models_last(x_inp, similarity_thred, 1)
+                    self._reduce_models_last(x_inp, 1)
                 else:
-                    tmp_losses = self._train_model(curr_model, x_inp, self.args.intm_epoch)
+                    tmp_losses = self._train_model(curr_model, x_inp, self._intm_epoch)
                     losses = losses + tmp_losses            
         except Exception as e:
-            print("At seed: ", self.arg.seed, "Error: ", e)
+            print("At seed: ", self.seed, "Error: ", e)
             return False, None, None
 
         return True, auc_hist, all_scores
