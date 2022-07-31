3eb3110c6dcdaca051a0850bf5c2b80e3fcc0841
e0jun <youngjun.lee@kaist.ac.kr>
2022-07-31 17:24:14 +0900

    [Add] data_utils: dataloader

--- datasets/data_utils.py
+++ datasets/data_utils.py
@@ -5,5 +5,6 @@ import tensorflow as tf
 def load_dataset(args):
     dataset = pd.read_csv("datasets/"+args.dataset_name+".csv", dtype=np.float64, header=None)
     dataset_label = dataset.pop(dataset.columns[-1])
-    args.input_dim = tf.data.Dataset.from_tensor_slices((dataset.values, dataset_label.values)).element_spec[0].shape[0]
-    return args, tf.data.Dataset.from_tensor_slices((dataset.values, dataset_label.values))
+    loader = tf.data.Dataset.from_tensor_slices((dataset.values, dataset_label.values))
+    args.input_dim = loader.element_spec[0].shape[0]
+    return args, loader
